6963393e71dcc93fcdd96827dcf4986c5a103cdf
RUPESH  KUMAR <118551823+Rupeshkumar96@users.noreply.github.com>
2026-05-14 22:29:56 +0530

    Hash Table
    Hash table using linear probing to resolve collisions

---
+++ 01_linear_probing.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int hashTable[10];
+    for(int i=0;i<10;i++) hashTable[i] = -1;
+
+    int arr[] = {23, 43, 13, 27};
+    for(int i=0;i<4;i++) {
+        int key = arr[i];
+        int index = key % 10;
+
+        while(hashTable[index] != -1)
+            index = (index + 1) % 10;
+
+        hashTable[index] = key;
+    }
+
+    for(int i=0;i<10;i++)
+        cout << i << " -> " << hashTable[i] << endl;
+}
