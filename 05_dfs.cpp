25fe16dad281df4da3c599396009a8f09121b7fb
RUPESH  KUMAR <118551823+Rupeshkumar96@users.noreply.github.com>
2026-05-18 23:03:09 +0530

    Update the Code

--- 05_dfs.cpp
+++ 05_dfs.cpp
@@ -1,25 +1,66 @@
+// dfs_matrix.cpp
 #include <iostream>
+#include <stack>
 using namespace std;
 
-void dfs(int graph[4][4], bool visited[], int node) {
-    visited[node] = true;
-    cout << node << " ";
+class Graph {
+private:
+    int vertices;
+    int** adjMatrix;
 
-    for(int i=0;i<4;i++) {
-        if(graph[node][i] && !visited[i])
-            dfs(graph, visited, i);
+    void DFSUtil(int vertex, bool* visited) {
+        visited[vertex] = true;
+        cout << vertex << " ";
+
+        for (int i = 0; i < vertices; i++) {
+            if (adjMatrix[vertex][i] == 1 && !visited[i]) {
+                DFSUtil(i, visited);
+            }
+        }
     }
-}
+
+public:
+    Graph(int v) {
+        vertices = v;
+        adjMatrix = new int*[vertices];
+        for (int i = 0; i < vertices; i++) {
+            adjMatrix[i] = new int[vertices];
+            for (int j = 0; j < vertices; j++) {
+                adjMatrix[i][j] = 0;
+            }
+        }
+    }
+
+    void addEdge(int u, int v) {
+        adjMatrix[u][v] = 1;
+        adjMatrix[v][u] = 1; // For undirected graph
+    }
+
+    void DFS(int startVertex) {
+        bool* visited = new bool[vertices];
+        for (int i = 0; i < vertices; i++) {
+            visited[i] = false;
+        }
+
+        cout << "DFS Traversal starting from vertex " << startVertex << ": ";
+        DFSUtil(startVertex, visited);
+        cout << endl;
+
+        delete[] visited;
+    }
+};
 
 int main() {
-    int graph[4][4] = {
-        {0,1,1,0},
-        {1,0,1,1},
-        {1,1,0,0},
-        {0,1,0,0}
-    };
+    Graph g(6);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
+    g.addEdge(1, 4);
+    g.addEdge(2, 4);
+    g.addEdge(3, 4);
+    g.addEdge(3, 5);
 
-    bool visited[4] = {false};
+    g.DFS(0);
 
-    dfs(graph, visited, 0);
+    return 0;
 }
