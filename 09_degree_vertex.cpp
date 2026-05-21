2b5603000587193fa26c6a9eccc09b710a78a5a7
RUPESH  KUMAR <118551823+Rupeshkumar96@users.noreply.github.com>
2026-05-22 00:49:23 +0530

    Add files via upload

---
+++ 09_degree_vertex.cpp
@@ -0,0 +1,52 @@
+// vertex_degree.cpp
+#include <iostream>
+using namespace std;
+
+class Graph {
+private:
+    int vertices;
+    int** adjMatrix;
+
+public:
+    Graph(int v) {
+        vertices = v;
+        adjMatrix = new int*[vertices];
+        for (int i = 0; i < vertices; i++) {
+            adjMatrix[i] = new int[vertices];
+            for (int j = 0; j < vertices; j++) {
+                adjMatrix[i][j] = 0;
+            }
+        }
+    }
+
+    void addEdge(int u, int v) {
+        adjMatrix[u][v] = 1;
+        adjMatrix[v][u] = 1;
+    }
+
+    void findDegrees() {
+        cout << "Degrees of vertices:" << endl;
+        for (int i = 0; i < vertices; i++) {
+            int degree = 0;
+            for (int j = 0; j < vertices; j++) {
+                if (adjMatrix[i][j] == 1) {
+                    degree++;
+                }
+            }
+            cout << "Vertex " << i << ": " << degree << endl;
+        }
+    }
+};
+
+int main() {
+    Graph g(5);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
+    g.addEdge(1, 4);
+    g.addEdge(2, 3);
+
+    g.findDegrees();
+
+    return 0;
+}
