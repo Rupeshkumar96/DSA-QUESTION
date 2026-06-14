0fd11e5f8fabb3f1288893ee9f5ea570c2121adf
RUPESH  KUMAR <118551823+Rupeshkumar96@users.noreply.github.com>
2026-06-14 22:22:06 +0530

    Binary search tree cod

---
+++ 33_bst_search.cpp
@@ -0,0 +1,85 @@
+// bst_search.cpp
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class BST {
+private:
+    Node* root;
+
+    Node* insertRecursive(Node* node, int value) {
+        if (node == NULL) {
+            return new Node(value);
+        }
+
+        if (value < node->data) {
+            node->left = insertRecursive(node->left, value);
+        } else if (value > node->data) {
+            node->right = insertRecursive(node->right, value);
+        }
+
+        return node;
+    }
+
+    bool searchRecursive(Node* node, int value) {
+        if (node == NULL) {
+            return false;
+        }
+
+        if (value == node->data) {
+            return true;
+        } else if (value < node->data) {
+            return searchRecursive(node->left, value);
+        } else {
+            return searchRecursive(node->right, value);
+        }
+    }
+
+public:
+    BST() {
+        root = NULL;
+    }
+
+    void insert(int value) {
+        root = insertRecursive(root, value);
+    }
+
+    bool search(int value) {
+        return searchRecursive(root, value);
+    }
+};
+
+int main() {
+    BST tree;
+
+    tree.insert(50);
+    tree.insert(30);
+    tree.insert(70);
+    tree.insert(20);
+    tree.insert(40);
+    tree.insert(60);
+    tree.insert(80);
+
+    int keys[] = {40, 90, 60, 25};
+
+    for (int key : keys) {
+        if (tree.search(key)) {
+            cout << key << " found in BST" << endl;
+        } else {
+            cout << key << " not found in BST" << endl;
+        }
+    }
+
+    return 0;
+}
